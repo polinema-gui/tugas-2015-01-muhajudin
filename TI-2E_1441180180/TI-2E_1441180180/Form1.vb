f49d60855ed2e308686adf3fa80d6873727a9e84
Muhajudin <muhajudin@gmail.com>
2015-10-02 15:02:54 +0700

    Tugas 1

---
+++ TI-2E_1441180180/TI-2E_1441180180/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Me.BackColor = Color.Chocolate Then
+            Me.BackColor = Color.Aquamarine
+        ElseIf Me.BackColor = Color.Aquamarine Then
+            Me.BackColor = Color.Chocolate
+        End If
+    End Sub
+End Class
